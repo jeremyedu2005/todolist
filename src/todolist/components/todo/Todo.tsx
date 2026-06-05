aacc07d33ae5af8fd5741aefd4bad36a5a44a20f
Adrien Loup <adloup@gmail.com>
2026-06-05 23:40:09 +0200

    ajout du composant todo

--- src/todolist/components/todo/Todo.tsx
+++ src/todolist/components/todo/Todo.tsx
@@ -1,16 +1,33 @@
-import type { TodoType } from '../../domain/TodolistType.ts';
+import { type SubmitEvent, useState } from 'react';
 import { useTodolistDispatch } from '../../infrastructure/useTodolist.ts';
-import { Button } from '../../../../common/components/button/Button.tsx';
+import { Button } from '../../../common/components/button/Button.tsx';
+import { Error } from '../../../common/components/error/Error.tsx';
+import type { TodoType } from '../../domain/TodolistType.ts';
 import styles from './Todo.module.scss';
 
-export const Todo = ({ todo }: { todo: TodoType }) => {
+export const Todo = ({ id, text, done }: TodoType) => {
   const dispatch = useTodolistDispatch();
+  const [todo, setTodo] = useState({ id, text });
+  const [isChange, setChange] = useState(false);
+  const [error, setError] = useState('');
 
-  const toggleTodo = (id: string) =>
+  const changeTodo = (event: SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (todo.text === '') {
+      setError('Veuillez saisir un texte pour ajouter une tâche.');
+      return;
+    }
+
     dispatch({
-      type: 'TOGGLE',
-      id: id,
+      type: 'CHANGE',
+      id: todo.id,
+      text: todo.text,
     });
+
+    setChange(false);
+    setError('');
+  };
 
   const removeTodo = (id: string) =>
     dispatch({
@@ -18,21 +35,43 @@ export const Todo = ({ todo }: { todo: TodoType }) => {
       id: id,
     });
 
+  const toggleTodo = (id: string) =>
+    dispatch({
+      type: 'TOGGLE',
+      id: id,
+    });
+
   return (
-    <div className={todo.done ? styles.done : ''}>
+    <div className={[styles.todo, done ? ` ${styles.done}` : ''].join('')}>
       <input
         type="checkbox"
-        id={todo.id}
-        checked={todo.done}
-        onChange={() => toggleTodo(todo.id)} /* Change l’état de l’élement grâce à la clé id. */
+        id={id}
+        checked={done}
+        onChange={() => toggleTodo(id)} /* Change l’état de l’élement grâce à la clé id. */
       />
-      <label
-        htmlFor={todo.id}
-        className={styles.label}
-      >
-        {todo.text}
-      </label>
-      <Button onClick={() => removeTodo(todo.id)} /* Supprime l’élement grâce à la clé id. */>Supprimer</Button>
+      {isChange ? (
+        <form onSubmit={changeTodo}>
+          <input
+            type="type"
+            id={id}
+            value={todo.text}
+            onChange={(e) => setTodo({ id: id, text: e.target.value })}
+          />
+          <Button type="submit">Enregistrer</Button>
+          <Error error={error} />
+        </form>
+      ) : (
+        <>
+          <label
+            htmlFor={id}
+            className={styles.label}
+          >
+            {todo.text}
+          </label>
+          <Button onClick={() => setChange(true)}>Modifier</Button>
+        </>
+      )}
+      <Button onClick={() => removeTodo(id)} /* Supprime l’élement grâce à la clé id. */>Supprimer</Button>
     </div>
   );
 };
